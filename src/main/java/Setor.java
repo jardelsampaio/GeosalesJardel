6631d1185d0df60de4561d19bdae7efe5804d8ec
jardelsampaio <jardel.sampaio@geosales.com.br>
2022-08-13 12:43:46 -0300

    criando crud

--- src/main/java/Setor.java
+++ src/main/java/Setor.java
@@ -1,7 +1,8 @@
 public class Setor {
+    //atributos
     String nome;
     int telefone;
-
+    //contrutor
     public Setor(String nome, int telefone){
         this.nome = nome;
         this.telefone = telefone;
--- src/main/java/TesteJardel.java
+++ src/main/java/TesteJardel.java
@@ -1,9 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TesteJardel {
     public static void main(String[] args) {
-        System.out.println("Geosales");
-        Setor geo = new Setor("CustomerService",8596);
-        System.out.println("setor: " + geo.nome +"\n"+ "telefone: " + geo.telefone);
-        Setor geo1 = new Setor("Projetos",7687);
-        System.out.println("setor: " + geo1.nome +"\n"+ "telefone: " + geo1.telefone);
+        Scanner leia = new Scanner(System.in);
+        //String atualiza = "Jardel";
+        //System.out.println(atualiza);
+        //atualiza = "sampaio";
+        //System.out.println(atualiza);
+        ArrayList<Setor> lista = new ArrayList<Setor>();
+
+
+        char controle = 's';
+        do {
+            System.out.println("Digite o nome: ");
+            String n = leia.next();
+            System.out.println("Digite o telefone");
+            int  i = leia.nextInt();
+            Setor geo = new Setor(n,i);
+            lista.add(geo);
+            System.out.println("Deseja cadastrar outro setor? s ou S para sim");
+            controle = leia.next().charAt(0);
+        }while(controle == 's' || controle == 'S');
+
+        do {
+            System.out.println("Deseja Atualizar o Setor? s ou S para sim");
+            controle = leia.next().charAt(0);
+            System.out.println("Digite o novo nome: ");
+            String n = leia.next();
+            System.out.println("Digite o novo telefone");
+            int  i = leia.nextInt();
+        }while(controle == 's' || controle == 'S');
+
+        do{
+            System.out.println("Deseja deletar o Setor? s ou S para sim");
+            controle = leia.next().charAt(0);
+            lista.remove(0);
+        }while(controle == 's' || controle == 'S');
+
+        for (Setor i: lista){
+            System.out.println(i);
+        }
     }
 }
